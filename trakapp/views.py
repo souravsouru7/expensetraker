07cff4b1d5c2bac68d5ab7072ba4dd87267eb8de
sourav <soutavr5@gmail.com>
2023-06-18 12:29:36 +0530

    firstime

---
+++ trakapp/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render,redirect
+from .forms import ExpenseForm
+from .models import Expense
+from django.db.models import Sum
+import datetime
+
+def index(request):
+    if request.method =="POST":
+        expense = ExpenseForm(request.POST)
+        if expense.is_valid():
+            expense.save()
+            
+    expenses = Expense.objects.all()
+    total_exp = expenses.aggregate(total=Sum('amount'))
+
+
+    last_year = datetime.date.today() - datetime.timedelta(days=365)
+    yearly_expenses = Expense.objects.filter(date__gt=last_year)
+    yearly_sum = yearly_expenses.aggregate(total=Sum('amount'))
+
+    last_month = datetime.date.today() - datetime.timedelta(days=30)
+    monthly_expenses = Expense.objects.filter(date__gt=last_month)
+    monthly_sum = monthly_expenses.aggregate(total=Sum('amount'))
+
+    last_week = datetime.date.today() - datetime.timedelta(days=7)
+    weekly_expenses = Expense.objects.filter(date__gt=last_week)
+    weekly_sum = weekly_expenses.aggregate(total=Sum('amount'))
+
+   
+    expense_form = ExpenseForm()
+    return render(request,'trakapp/index.html',{'expense_form':expense_form,'expenses':expenses,"total_exp": total_exp,"yearly_sum": yearly_sum,"monthly_sum":monthly_sum,"weekly_sum": weekly_sum})
+
+def edit(request,id):
+    expense=Expense.objects.get(id=id)
+    
+    expense_form = ExpenseForm(instance=expense)
+    if request.method =="POST":
+        expense=Expense.objects.get(id=id)
+        form =ExpenseForm(request.POST,instance=expense)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    return render(request,"trakapp/edit.html",{"expense_form": expense_form})
+
+
+def delete(request,id):
+    if request.method == 'POST' and 'delete' in request.POST:
+        expense=Expense.objects.get(id=id)
+        expense.delete()
+    return redirect('index')
